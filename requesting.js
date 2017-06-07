0ec4aaa56c0464f10d18b674ba61fb9159759bb8
Skywawe <lucas.addisi@gmail.com>
2017-06-07 20:32:12 -0300

    Minnor changes

--- requesting.js
+++ requesting.js
@@ -1,19 +1,15 @@
+var URL = "https://fastspeedster.herokuapp.com/api/tracks/42";
+
 var request = function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+            callback();
+          }
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous
     xmlHttp.send(null);
 }
 
-var callback = function(str){
-  console.log(str);
-}
-
-var URL = "https://fastspeedster.herokuapp.com/api/tracks";
-/*var URLArray = ["https://fastspeedster.herokuapp.com/api/tracks", "https://fastspeedster.herokuapp.com/api/runners",
- "https://fastspeedster.herokuapp.com/api/webcams"]; */
-request(URL, callback);
+request(URL);
