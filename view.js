0e0cc92662ef451f23e08382d13b1481c646eefc
lucasaddisi <lucas.addisi@gmail.com>
2017-06-07 21:58:09 -0300

    Now track is printed

--- view.js
+++ view.js
@@ -1,13 +1,35 @@
 function initMap (){
+
+
+	var mapa = L.map('mapid').setView([-34.5221554, -58.7000067], 13);
+
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(mapa);
+
+
+
 	var URL = "https://fastspeedster.herokuapp.com/api/";
 
-	var req1 = $.ajax(URL + "tracks/42");
-	var req2 = $.ajax(URL + "runners");
+	var trackInfo = $.ajax(URL + "tracks/42");
+	//var runnersInfo = $.ajax(URL + "runners");
 
-	$.when(req1, req2).done(function(res1, res2){
-	      console.log(req1);
-	      console.log(req2);
+	$.when(trackInfo).done(function(tracksResponse){
+
+		console.log(tracksResponse["track"]["coordinates"]);
+
+		var trackPoints = tracksResponse["track"]["coordinates"].map(p => L.latLng(p.lat, p.lon)); //for(point in tracksResponse[0]){}
+
+		console.log(trackPoints);
+
+		L.polygon(trackPoints).addTo(mapa);
+
 	});
+
+//	$.when(req1, req2).done(function(res1, res2){
+//	      console.log(req1);
+//	      console.log(req2);
+//	});
 
 }
 
