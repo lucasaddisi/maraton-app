6137de6fae5f86c677995f7e30766efdc8a6e713
leafunes <funes.s.leandro@gmail.com>
2017-06-08 02:08:06 -0300

    Se organizo un poco el codigo de view.js

--- view.js
+++ view.js
@@ -1,35 +1,28 @@
 function initMap (){
 
 
-	var mapa = L.map('mapid').setView([-34.5221554, -58.7000067], 13);
+	var mapa = L.map('mapid');
+	var URL = "https://fastspeedster.herokuapp.com/api/";
+	var trackInfo = $.ajax(URL + "tracks/42");
+	var runnersInfo = $.ajax(URL + "runners");
+
+	var runners = [];
 
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 		}).addTo(mapa);
 
 
+	trackInfo.done(function(tracksResponse){
 
-	var URL = "https://fastspeedster.herokuapp.com/api/";
+		var trackPoints = tracksResponse["track"]["coordinates"].map(p => L.latLng(p.lat, p.lon));
+		var polygon = L.polygon(trackPoints).addTo(mapa);
 
-	var trackInfo = $.ajax(URL + "tracks/42");
-	//var runnersInfo = $.ajax(URL + "runners");
-
-	$.when(trackInfo).done(function(tracksResponse){
-
-		console.log(tracksResponse["track"]["coordinates"]);
-
-		var trackPoints = tracksResponse["track"]["coordinates"].map(p => L.latLng(p.lat, p.lon)); //for(point in tracksResponse[0]){}
-
-		console.log(trackPoints);
-
-		L.polygon(trackPoints).addTo(mapa);
+		mapa.setView(polygon.getBounds().getCenter(), 17);
 
 	});
 
-//	$.when(req1, req2).done(function(res1, res2){
-//	      console.log(req1);
-//	      console.log(req2);
-//	});
+
 
 }
 
