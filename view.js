2e8fb4aac4f8ab37aeb0758834cba0ab0a6f9d8b
Skywawe <lucas.addisi@gmail.com>
2017-06-07 21:35:39 -0300

    requesting deleted

--- view.js
+++ view.js
@@ -1,35 +1,14 @@
 function initMap (){
+	var URL = "https://fastspeedster.herokuapp.com/api/";
 
-	var track = new Track();
+	var req1 = $.ajax(URL + "tracks/42");
+	var req2 = $.ajax(URL + "runners");
 
-	var trackpoints = track.getPoints();
+	$.when(req1, req2).done(function(res1, res2){
+	      console.log(req1);
+	      console.log(req2);
+	});
 
-	var ungsLoc = [-34.5221554, -58.7000067];
-	var map = L.map('mapid').setView(ungsLoc, 15);
-
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
-
-	var latLongpoints = trackpoints.map(function(p){ L.latLng(p.getLat(), p.getLng()) });
-
-	L.polygon (latLongpoints).addTo(map);
-	
-
-/*
-var circle = L.circle(ungsLoc, {
-	color: '#0000AA',
-	fillColor: '#0000CC',
-	fillOpacity: 0.2,
-	radius: 300
-}).addTo(map);
-
-L.polygon ([
-	L.latLng (-34.532596, -58.701761),
-	L.latLng (-34.537671, -58.718153),
-	L.latLng (-34.527991, -58.707927),
-	L.latLng (-34.542229, -58.712136),
-	]).addTo(map);
 }
-*/
+
 $(initMap);
