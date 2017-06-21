72c355058ecb9a93abb5179b2357939528d0470b
lucasaddisi <lucas.addisi@gmail.com>
2017-06-21 11:58:51 -0300

    Runner icons added

--- resources/js/map.js
+++ resources/js/map.js
@@ -13,33 +13,44 @@ var Mapa = function (mapID) {
 
 	}
 
+
+	this.icon = function(url){
+		var ret = L.icon({
+		    iconUrl: url,
+		    iconSize:     [20, 20], // size of the icon
+		    iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
+		    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+				});
+		return ret;
+	}
+
 	this.drawTrack = function(track){
 
 		var coordinates = track.points.map(p => L.latLng(p.lat, p.lng));
     var ruta = L.polyline(coordinates, {color: "#F00"}).addTo(this.mapa);
-		var ungsLocation = [-34.5221554, -58.7000067];
+		var ungsLocation = [-34.5228554, -58.698];
 		this.mapa.setView(ungsLocation, 17);
 
 	}
 
 	this.drawRunner = function(runner){
-		L.marker([runner.positions[0].lat, runner.positions[0].lng])
+
+		var info = "Runner Name: " + runner.name + " " + runner.surname +
+							 "\n Sponsor Name: " + runner.sponsor.name;
+
+		L.marker([runner.positions[0].lat, runner.positions[0].lng],
+	 	{icon: this.icon('\images\\ic_android_black_24dp_2x.png')})
+		.bindPopup(info)
 		.addTo(this.mapa);
 	}
 
 	this.drawCamera = function(camera){
 
-		var cameraIcon = L.icon({
-    iconUrl: '\images\\ic_camera_enhance_black_24dp_1x.png',
+		var info = "Camera ID: " + camera.id;
 
-    iconSize:     [20, 20], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-		});
-
-		L.marker([camera.coordinate.lat, camera.coordinate.lon], {icon: cameraIcon})
+		L.marker([camera.coordinate.lat, camera.coordinate.lon],
+	 	{icon: this.icon('\images\\ic_camera_enhance_black_24dp_2x.png')})
+		.bindPopup(info)
 		.addTo(this.mapa);
 	}
 
